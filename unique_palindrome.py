67235c2fc456a77bcd30c8929ea456a8c3a8dabe
pavan kumar (202102) <pavanponnaganti2002@gmail.com>
2022-08-19 22:11:09 +0530

    comments

--- unique_palindrome.py
+++ unique_palindrome.py
@@ -1,20 +1,20 @@
 class palindrome:
-    def _init_(self,string):
+    def __init__(self,string):
         self.str1=string
         self.unique_set=set()
-    def substring(self):
+    def substring(self):   #to make substrings
         i = 0
         while i <len(self.str1):
             j = len(self.str1) - 1
             while (j>i):
-                if self.str1[i] == self.str1[j]:
+                if self.str1[i] == self.str1[j]: #if first and last are equal then it will cal palindrome for cerification
                     if self.is_palindrome(i,j):
-                        self.unique_set.add(self.str1[i:j+1])
+                        self.unique_set.add(self.str1[i:j+1])   #set contain all unique elements
                         i = (i+j)//2
                         break
                 j -= 1
             i+=1
-    def is_palindrome(self,strt,end):
+    def is_palindrome(self,strt,end):   #palindrome verification
         while(strt<=end):
             if(self.str1[strt]==self.str1[end]):
                 strt+=1
